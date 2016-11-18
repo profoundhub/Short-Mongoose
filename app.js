c7911e0d98b3ad73497bb2bb8b169094d427601b
Daniel Lim <hbt@markhamboards.com>
2016-11-18 14:03:32 -0500

    update Daniel Updating
    Daniel Updating

--- app.js
+++ app.js
@@ -9,3 +9,14 @@ var path = require('path');
 var fs = require('fs');
 
 var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
